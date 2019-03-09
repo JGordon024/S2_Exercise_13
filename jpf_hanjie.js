0024a36ebae4209f7c349bf415f1c5abbc0d21f2
Unknown <jgordon024@west-mec.org>
2019-03-08 18:13:53 -0700

    Clicking
    Added the ability to click boxes and drag the mouse to select boxes. Made a few functions to achieve this.

--- jpf_hanjie.js
+++ jpf_hanjie.js
@@ -57,6 +57,7 @@
 window.onload = init;
 
 var puzzleCells;
+var cellBackground;
 
 function init() {
       //insert the title for the first puzzle
@@ -71,6 +72,9 @@ function init() {
       }
       setupPuzzle();
 }
+
+//Add an event listener for the mouseup event
+document.addEventListener("mouseup", endBackground);
 
 function swapPuzzle(e) {
       //retrieve the ID of the clicked button
@@ -94,16 +98,39 @@ function swapPuzzle(e) {
       setupPuzzle();
 }
 
+
 function setupPuzzle() {
       //match all of the datacells in the puzzle
       puzzleCells = document.querySelectorAll("table#hanjieGrid td")
       //Set the initial color of each cell to gold
       for (var i = 0; i < puzzleCells.length; i++) {
             puzzleCells[i].style.backgroundColor = "rgb(233,207, 29)";
+            //Set the cell background color in response to the mousedown event
+            puzzleCells[i].onmousedown = setBackground;
       }
 }
 
 
+function setBackground(e) {
+      cellBackground = "rgb(101, 101, 101";
+      e.target.style.backgroundColor = cellBackground
+      //Create an event listener for every puzzle cell
+      for (var i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].addEventListener("mouseenter", extendBackground)
+      }
+}
+
+//Create a fuction to extend the background and set a new background color
+function extendBackground(e) {
+      e.target.style.backgroundColor = cellBackground;
+}
+
+function endBackground() {
+      //remove the event listener for every puzzle cell
+      for (var i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].removeEventListener("mouseenter", extendBackground);
+      }
+}
 
 /* ================================================================= */
 
